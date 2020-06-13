076ea43596e39399aa5778ba9d20c6cbc4d3d840
n4beel <nabeel3353045805@hotmail.com>
2020-06-13 11:39:11 +0500

    made error

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacts Again
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Reacts Again
         </a>
       </header>
     </div>
